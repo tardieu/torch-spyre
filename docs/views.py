db0e742bd9e4b9188d2d02b112354dc0ff3b84de
Olivier Tardieu <tardieu@us.ibm.com>
2026-03-14 00:53:04 -0400

    Update view code

--- docs/views.py
+++ docs/views.py
@@ -1,191 +1,136 @@
 import sympy
+from typing import Sequence
 
 
-class Tensor:
-    def __init__(self, size, stride, device_size, stride_map):
-        self.size = size
-        self.stride = stride
-        self.device_size = device_size
-        self.stride_map = stride_map
-        self.device_stride = [0] * len(self.device_size)  # to be computed
-        self.unpadded_size = [0] * len(self.size)  # to be computed
-
-        # compute device_stride
-        acc = 1
-        for dim in range(len(self.device_size) - 1, -1, -1):
-            if self.stride_map[dim] != 0:
-                self.device_stride[dim] = acc
-                acc = acc * self.device_size[dim]
-
-        # compute unpadded_size
-        for dim in range(len(self.size)):
-            top = max(self.stride) * self.size[dim]
-            for st in stride:
-                if st > stride[dim] and st < top:
-                    top = st
-            self.unpadded_size[dim] = top // self.stride[dim]
-
-    def h2d(self, h):
-        """map host coordinates to device coordinates"""
-        offset = 0
-        for dim, c in enumerate(h):
-            if c < 0 or c >= self.size[dim]:
-                raise IndexError
-            offset += c * self.stride[dim]
-        d = [0] * len(self.device_size)
-        for dim in range(len(self.device_size)):
-            if self.stride_map[dim] > 0:
-                d[dim] = offset // self.stride_map[dim] % self.device_size[dim]
-        return d
-
-    def d2h(self, d):
-        """map device coordinates to host coordinates"""
-        offset = 0
-        for dim, c in enumerate(d):
-            if c < 0 or c >= self.device_size[dim]:
-                raise IndexError
-            offset += c * max(0, self.stride_map[dim])
-        h = [0] * len(self.size)
-        for dim in range(len(self.size)):
-            h[dim] += offset // self.stride[dim] % self.unpadded_size[dim]
-        return h
-
-    def compute_coordinates(self, var_ranges, index):
-        """derive an array of coordinate expressions into a tensor from an index"""
-        coordinates = [sympy.S.Zero] * len(self.size)
-        vars = index.free_symbols
-        for var in vars:
-            if var_ranges[var] <= 1:
-                continue
-            term = index.subs({v: 0 for v in vars - {var}})
-            step = term.subs(var, 1)
-            limit = term.subs(var, var_ranges[var])
-            primary_stride = 0
-            primary_dim = -1
-            for dim in range(len(self.size)):
-                if self.size[dim] == 1:
-                    continue
-                st = self.stride[dim]
-                if st > step and st < limit:
-                    coordinates[dim] += var * step // st
-                elif st <= step and st > primary_stride:
-                    primary_stride = st
-                    primary_dim = dim
-            coordinates[primary_dim] += (
-                var * step // primary_stride  # % self.unpadded_size[primary_dim]
-            )
-        return coordinates
-
-    def compute_device_coordinates(self, var_ranges, index):
-        """derive an array of coordinate expressions into a device tensor from an index"""
-        coordinates = [sympy.S.Zero] * len(self.device_size)
-        vars = index.free_symbols
-        for var in vars:
-            if var_ranges[var] <= 1:
-                continue
-            term = index.subs({v: 0 for v in vars - {var}})
-            step = term.subs(var, 1)
-            limit = term.subs(var, var_ranges[var])
-            primary_stride = 0
-            primary_dim = -1
-            for dim in range(len(self.device_size)):
-                if self.device_size[dim] == 1:
-                    continue
-                st = self.stride_map[dim]
-                if st > step and st < limit:
-                    coordinates[dim] += var * step // st
-                elif st <= step and st > primary_stride:
-                    primary_stride = st
-                    primary_dim = dim
-            coordinates[primary_dim] += (
-                var * step // primary_stride  # % self.device_size[primary_dim]
-            )
-        return coordinates
+# A stride_map maps device dimensions to host strides
+# stride_map[dim] = -1 for dimensions that do not exist in the host tensor
+# stride_map[dim] = 0 is permitted to encode expanded dimensions
 
 
-print(vars(Tensor([512, 256], [384, 1], [196608], [1])))
+def compute_device_stride(
+    device_size: Sequence[sympy.Expr], stride_map: Sequence[sympy.Expr]
+):
+    """Compute device strides"""
+    device_stride = [0] * len(device_size)
+    current_stride = 1
+    for dim in range(len(device_size) - 1, -1, -1):
+        if stride_map[dim] != 0:  # dimensions with stride 0 require no memory
+            device_stride[dim] = current_stride
+            current_stride *= device_size[dim]
+    return device_stride
 
-print(vars(Tensor([512], [1], [512], [1])))
-print(vars(Tensor([512], [1], [512], [1])))
-print(vars(Tensor([4, 128], [128, 1], [4, 128], [128, 1])))
-print(vars(Tensor([4, 128], [128, 1], [2, 4, 64], [64, 128, 1])))
-print(vars(Tensor([3, 1, 256], [256, 256, 1], [1, 4, 3, 64], [256, 64, 256, 1])))
-print(vars(Tensor([3, 256], [256, 1], [1, 4, 3, 64], [256, 64, 256, 1])))
-print(vars(Tensor([1], [1], [1], [1])))
-print(vars(Tensor([1, 1], [1, 1], [1, 1], [1, 1])))
-print(vars(Tensor([1], [1], [1, 1], [1, 1])))
-print(vars(Tensor([4, 64], [64, 1], [1, 4, 64], [64, 64, 1])))
-print(vars(Tensor([4, 1], [1, 1], [1, 4, 64], [1, 1, -1])))
-print(vars(Tensor([4], [1], [64], [1])))
-print(vars(Tensor([4, 3], [3, 1], [1, 4, 64], [1, 3, 1])))
-print(vars(Tensor([4, 128], [128, 1], [2, 4, 64], [64, 128, 1])))
 
-print(Tensor([4, 1], [1, 1], [1, 4, 64], [1, 1, -1]).d2h([0, 2, 0]))
-print(Tensor([4, 3], [3, 1], [1, 4, 64], [1, 3, 1]).d2h([0, 2, 3]))
-print(Tensor([4, 128], [128, 1], [2, 4, 64], [64, 128, 1]).d2h([1, 3, 7]))
+def compute_dim_map(
+    size: Sequence[sympy.Expr],
+    stride: Sequence[sympy.Expr],
+    device_size: Sequence[sympy.Expr],
+    stride_map: Sequence[sympy.Expr],
+):
+    """Compute dim_map"""
+    max_stride_le = [0] * len(size)
+    dim_map = [-1] * len(device_size)
+    for i, hst in enumerate(stride):
+        if size[i] == 1:
+            continue  # dimensions of size 1 cannot reliably be mapped to hast dimensions
+        for j, dst in enumerate(stride_map):
+            if hst > max_stride_le[j] and hst <= dst:
+                max_stride_le[j] = hst
+                dim_map[j] = i
+    return dim_map
 
-print(Tensor([512, 256], [384, 1], [196608], [1]).d2h([255]))
 
-p0, p1, p2, p3 = sympy.symbols("p0 p1 p2 p3", integer=True)
+def compute_padded_size(size: Sequence[sympy.Expr], stride: Sequence[sympy.Expr]):
+    """Compute padded_size from size and stride"""
+    padded_size = [0] * len(size)
+    for dim in range(len(size)):
+        min_stride_gt = max(stride) * size[dim]  # outermost dim is not padded
+        for st in stride:
+            if st > stride[dim] and st < min_stride_gt:
+                min_stride_gt = st
+        padded_size[dim] = min_stride_gt // stride[dim]
+    return padded_size
 
-print(
-    Tensor(
-        [2, 256, 4096], [1048576, 4096, 1], [256, 64, 2, 64], [4096, 64, 1048576, 1]
-    ).compute_coordinates(
-        {p0: 2, p1: 32, p2: 256, p3: 128},
-        1048576 * p0 + 128 * p1 + 4096 * p2 + p3,
+
+def compute_coordinates(
+    padded_size: Sequence[sympy.Expr],
+    stride: Sequence[sympy.Expr],
+    var_ranges: dict[sympy.Symbol, sympy.Expr],
+    index: sympy.Expr,
+) -> list[sympy.Expr]:
+    """Compute an array of coordinate expressions from an index expression"""
+    coordinates = [sympy.S.Zero] * len(stride)
+    vars = index.free_symbols
+    for var in vars:
+        if var_ranges[var] <= 1:
+            continue  # ranges of size 1 cannot reliably be mapped to coordinates
+        term = index.subs({v: 0 for v in vars - {var}})
+        step = term.subs(var, 1)
+        limit = term.subs(var, var_ranges[var])
+        primary_stride = 0
+        primary_dim = -1
+        for dim in range(len(stride)):
+            if padded_size[dim] == 1:
+                continue  # dimensions of size 1 cannot reliably be mapped to coordinates
+            st = stride[dim]
+            if st > step and st < limit:
+                coordinates[dim] += var * step // st
+            elif st <= step and st > primary_stride:
+                primary_stride = st
+                primary_dim = dim
+        coordinates[primary_dim] += (
+            var * step // primary_stride % padded_size[primary_dim]
+        )
+    return coordinates
+
+
+if __name__ == "__main__":
+    p0, p1, p2, p3 = sympy.symbols("p0 p1 p2 p3", integer=True)
+
+    # B, S, E viewed as B, H, S, E/H on host
+    print(
+        compute_coordinates(
+            [2, 256, 4096],
+            [1048576, 4096, 1],
+            {p0: 2, p1: 32, p2: 256, p3: 128},
+            1048576 * p0 + 128 * p1 + 4096 * p2 + p3,
+        )
     )
-)
 
-print(
-    Tensor(
-        [2, 256, 4096], [1048576, 4096, 1], [256, 64, 2, 64], [4096, 64, 1048576, 1]
-    ).compute_device_coordinates(
-        {p0: 2, p1: 32, p2: 256, p3: 128},
-        1048576 * p0 + 128 * p1 + 4096 * p2 + p3,
+    # B, S, E viewed as B, H, S, E/H on device
+    print(
+        compute_coordinates(
+            [256, 64, 2, 64],
+            [4096, 64, 1048576, 1],
+            {p0: 2, p1: 32, p2: 256, p3: 128},
+            1048576 * p0 + 128 * p1 + 4096 * p2 + p3,
+        )
     )
-)
 
-
-print(
-    Tensor(
-        [2, 256, 4096], [1048576, 4096, 1], [256, 64, 2, 64], [4096, 64, 1048576, 1]
-    ).compute_coordinates(
-        {p0: 512, p1: 4096},
-        4096 * p0 + p1,
+    # B, S, E viewed as B*S, E on host
+    print(
+        compute_coordinates(
+            [2, 256, 4096],
+            [1048576, 4096, 1],
+            {p0: 512, p1: 4096},
+            4096 * p0 + p1,
+        )
     )
-)
 
-print(
-    Tensor(
-        [2, 256, 4096], [1048576, 4096, 1], [256, 64, 2, 64], [4096, 64, 1048576, 1]
-    ).compute_device_coordinates(
-        {p0: 512, p1: 4096},
-        4096 * p0 + p1,
+    # B, S, E viewed as B*S, E on device
+    print(
+        compute_coordinates(
+            [256, 64, 2, 64],
+            [4096, 64, 1048576, 1],
+            {p0: 512, p1: 4096},
+            4096 * p0 + p1,
+        )
     )
-)
 
-print(
-    Tensor(
-        [256, 4096], [4096, 1], [64, 256, 64], [64, 4096, 1]
-    ).compute_device_coordinates(
-        {p0: 256, p1: 4096, p2: 1024},
-        4096 * p0 + p1,
+    print(
+        compute_coordinates(
+            [3, 1, 128],
+            [128, 128, 1],
+            {p0: 3, p1: 128},
+            128 * p0 + p1,
+        )
     )
-)
-
-print(
-    Tensor(
-        [1024, 4096], [4096, 1], [64, 1024, 64], [64, 4096, 1]
-    ).compute_device_coordinates(
-        {p0: 256, p1: 4096, p2: 1024},
-        p1 + 4096 * p2,
-    )
-)
-
-print(
-    Tensor([4, 1], [1, 1], [1, 4, 64], [-1, 1, -1]).compute_device_coordinates(
-        {p0: 4},
-        p0,
-    )
-)
