c6cf3fa5422816e93e26d7a0fdb0f4f9c968cef6
Olivier Tardieu <tardieu@us.ibm.com>
2026-03-25 15:25:32 +0000

    Handle tensors with single element

--- torch_spyre/_inductor/views.py
+++ torch_spyre/_inductor/views.py
@@ -266,7 +266,7 @@ def align_tensors(var_ranges, tensors, op_it_space_splits={}):
     rank = 0
     for i, t in enumerate(new_tensors):
         if stick_dim[i] is None:
-            rank = max(rank, len(t["size"]))
+            rank = max(2, rank, len(t["size"]))
             continue
         found = 1
         for c, s in zip(t["coordinates"][:-1], t["size"][:-1]):
